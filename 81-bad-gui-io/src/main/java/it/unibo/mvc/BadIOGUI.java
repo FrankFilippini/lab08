eecbf4c198b335ce25c02480da619c15c64e5ea0
Francesco <francesco.filippini8@studio.unibo.it>
2022-11-16 11:11:56 +0100

    First excercise completed

--- 81-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 81-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -10,12 +10,15 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+//import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.List;
+//import java.nio.file.Files;
+//import java.util.List;
 import java.util.Random;
 
 /**
@@ -43,6 +46,18 @@ public class BadIOGUI {
         canvas.setLayout(new BorderLayout());
         final JButton write = new JButton("Write on file");
         canvas.add(write, BorderLayout.CENTER);
+        final JPanel horizontal = new JPanel();
+        final JButton read = new JButton();
+        read.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                System.out.println("Pulsante premuto."); //NOPMD
+            }
+        });
+        horizontal.add(read);
+        horizontal.setLayout(new BoxLayout(horizontal, BoxLayout.X_AXIS));
+        canvas.add(horizontal, BorderLayout.CENTER);
+        horizontal.add(write, BorderLayout.CENTER);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
@@ -63,6 +78,17 @@ public class BadIOGUI {
                 } catch (IOException e1) {
                     JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
                     e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                }
+            }
+        });
+        read.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try (DataInputStream i = new DataInputStream(new FileInputStream(PATH))) {
+                    System.out.println(i.readLine()); //NOPMD
+                } catch (IOException e2) {
+                    JOptionPane.showMessageDialog(frame, e2, "Error", JOptionPane.ERROR_MESSAGE);
+                    e2.printStackTrace(); // NOPMD: allowed as this is just an exercise
                 }
             }
         });
@@ -87,6 +113,7 @@ public class BadIOGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.pack();
         /*
          * OK, ready to push the frame onscreen
          */
